e469418d8774e2de1289e669cd45855fc9c9d5cc
shirui <shirui@gmail.com>
2022-08-22 18:53:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const PGYERAppUploader = require('./PGYERAppUploader');
+
+try {
+  const apkKey = core.getInput('_api_key');
+  const appFilePath = core.getInput('appFilePath');
+  const buildInstallType = core.getInput('buildInstallType');
+  const buildPassword = core.getInput('buildPassword');
+
+  // get file ext
+  const ext = appFilePath.split('.').pop().toLowerCase();
+  const builtType = 'android';
+  if (ext == 'ipa') {
+    builtType = 'ios';
+  }
+
+  const uploader = new PGYERAppUploader(apkKey);
+  const uploadOptions = {
+    buildType: builtType,
+    filePath: appFilePath,
+    log: true,
+    buildInstallType: buildInstallType,
+    buildPassword: buildPassword
+  }
+
+  uploader.upload(uploadOptions).then(function(info) {
+    core.setOutput("info", info);
+  }).catch(console.error);
+
+} catch (error) {
+  core.setFailed(error.message);
+}
