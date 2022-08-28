3eb868beb47e9688c6ad3e050b4b1d05795ea02e
shirui <shirui@gmail.com>
2022-08-28 21:51:26 +0800

    add more params support

--- dist/index.js
+++ dist/index.js
@@ -11442,33 +11442,53 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(8349);
 const github = __nccwpck_require__(96);
-
 const PGYERAppUploader = __nccwpck_require__(5248);
 
 try {
-  const apkKey = core.getInput('_api_key');
-  const appFilePath = core.getInput('appFilePath');
-  const buildInstallType = core.getInput('buildInstallType');
-  const buildPassword = core.getInput('buildPassword');
-
-  // get file ext
-  const ext = appFilePath.split('.').pop().toLowerCase();
-  let builtType = 'android';
-  if (ext == 'ipa') {
-    builtType = 'ios';
-  }
-
-  const uploader = new PGYERAppUploader(apkKey);
   const uploadOptions = {
-    buildType: builtType,
-    filePath: appFilePath,
     log: false,
-    buildInstallType: buildInstallType,
-    buildPassword: buildPassword
   }
 
-  uploader.upload(uploadOptions).then(function(info) {
-    core.setOutput("info", info);
+  const apiKey = core.getInput('_api_key', { required: true });
+  if (!apiKey) {
+    core.warning('apiKey was not set');
+  }
+
+  const appFilePath = core.getInput('appFilePath', { required: true });
+  if (!appFilePath) {
+    core.warning('appFilePath was not set');
+  }
+  uploadOptions.filePath = appFilePath;
+
+  const otherParams = [
+    "buildInstallType",
+    "buildPassword",
+    "buildUpdateDescription",
+    "buildInstallDate",
+    "buildInstallStartDate",
+    "buildInstallEndDate",
+    "buildChannelShortcut"
+  ];
+
+  otherParams.forEach(name => {
+    let value = core.getInput(name);
+    if (value) {
+      uploadOptions[[name]] = value;
+    }
+  });
+
+  const ext = appFilePath.split('.').pop().toLowerCase();
+  if (ext == 'ipa') {
+    uploadOptions.buildType = 'ios';
+  } else if (ext == 'apk') {
+    uploadOptions.buildType = 'android';
+  } else {
+    core.warning(`Unsupported file type: ${ext}`);
+  }
+
+  const uploader = new PGYERAppUploader(apiKey);
+  uploader.upload(uploadOptions).then(function (info) {
+    core.setOutput("app", info);
   }).catch(console.error);
 
 } catch (error) {
